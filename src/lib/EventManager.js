a678b2e6935371912f613bcb351914d3316e44b0
vinicius <vinicius.henrique.92@hotmail.com>
2024-01-03 10:30:28 -0300

    feat: created the eventEmitter

---
+++ src/lib/EventManager.js
@@ -0,0 +1,56 @@
+export default class EventManager {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(listener);
+  }
+
+  emit(event, payload) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(payload);
+    });
+  }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners[event];
+
+    if (!listeners) {
+      return;
+    }
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove,
+    );
+
+    this.listeners[event] = filteredListeners;
+  }
+}
+
+const toastEventManager = new EventManager();
+
+function addToast1(payload) {
+  console.log('addtoast listener1', payload);
+}
+
+function addToast2(payload) {
+  console.log('addtoast listener2', payload);
+}
+
+toastEventManager.on('addtoast', addToast1);
+toastEventManager.on('addtoast', addToast2);
+
+toastEventManager.emit('addtoast', { type: 'danger', text: 'texto' });
+
+toastEventManager.removeListener('addtoast', addToast1);
+
+toastEventManager.emit('addtoast', 'depois de remover...');
+console.log(toastEventManager);
