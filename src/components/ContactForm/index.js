2afe04dac7b4464cbcfd3988082724ba2b1a7ccf
vinicius <vinicius.henrique.92@hotmail.com>
2024-01-04 15:54:27 -0300

    feat: cleaning the form fields after new contact

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -53,6 +53,12 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
       setPhone(formatPhone(contact.phone ?? ''));
       setCategoryId(contact.category_id ?? '');
     },
+    resetFields: () => {
+      setName('');
+      setEmail('');
+      setPhone('');
+      setCategoryId('');
+    },
   }), []);
 
   function handleNameChange(event) {
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,11 @@
+import { useRef } from 'react';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -13,6 +15,8 @@ export default function NewContact() {
         category_id: formData.categoryId,
       };
       await ContactsService.createContact(contact);
+
+      contactFormRef.current.resetFields();
 
       toast({
         type: 'success',
@@ -28,7 +32,11 @@ export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm onSubmit={handleSubmit} buttonLabel="Cadastrar" />
+      <ContactForm
+        ref={contactFormRef}
+        onSubmit={handleSubmit}
+        buttonLabel="Cadastrar"
+      />
     </>
   );
 }
