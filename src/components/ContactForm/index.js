d900565064348b3ea3a8dc18fc7ceda07fcb7691
vinicius <vinicius.henrique.92@hotmail.com>
2024-01-05 15:07:21 -0300

    fix: resolving warning of async state on unmounted component

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -13,14 +13,15 @@ import { Form, ButtonContainer } from './styles';
 import FormGroup from '../FormGroup';
 import formatPhone from '../../utils/formatPhone';
 import CategoriesService from '../../services/CategoriesService';
+import useSafeAsyncState from '../../hooks/useSafeAsyncState';
 
 const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [categories, setCategories] = useSafeAsyncState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const {
@@ -42,7 +43,7 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     }
 
     loadCategories();
-  }, []);
+  }, [setCategories, setIsLoadingCategories]);
 
   const isFormValid = (name && errors.length === 0);
 
