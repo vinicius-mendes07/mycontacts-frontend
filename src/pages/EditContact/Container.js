10414edac1e33a7a488fcc8d23aa9d574f7ec55a
vinicius <vinicius.henrique.92@hotmail.com>
2024-01-09 15:50:30 -0300

    feat: using Container/Representational Pattern

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+
+import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import Presentation from './Presentation';
+
+export default function Container() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contactData);
+          setIsLoading(false);
+          setContactName(contactData.name);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  async function handleSubmit(contact) {
+    try {
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+  return (
+    <Presentation
+      isLoading={isLoading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
